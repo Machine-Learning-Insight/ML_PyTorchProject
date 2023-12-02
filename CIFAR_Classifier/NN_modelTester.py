e76e230e0d0b5d2ac38e273f95c8217e7a4acf36
larezelliot <elliotlarez02@gmail.com>
2023-12-01 19:21:28 -0500

    Refactor

---
+++ CIFAR_Classifier/NN_modelTester.py
@@ -0,0 +1,68 @@
+# https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+import time
+
+import torch
+import numpy as np
+from torch import nn
+
+import helper_CIFAR10
+import SimpleNN as nn_CIFAR10
+# import ssl
+
+# TODO: ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def test_model(nn_model: nn.Module, testing_loader, classes_number):
+
+    print(f'Testing model epochs...')
+
+    prediction_matrix = np.zeros((classes_number, classes_number))
+
+    last = time.time()
+    with torch.no_grad():
+        for load in testing_loader:
+            # Input: unloads input data and label
+            inputs, labels = load
+
+            # Prediction: generates output
+            output = nn_model(inputs)
+
+            # the class with the highest energy is what we choose as prediction
+            max_value, predictions = torch.max(output.data, 1)
+
+            prediction_matrix[labels][predictions] += 1
+
+    now = time.time()
+    print(f'Finished Testing in {now-last} seconds')
+
+    return prediction_matrix
+
+
+def main():
+    print('NN_modelTester.py\n')
+    dataset_choice = int(input('Select a data set\n' +
+                               '1. CIFAR10\n' +
+                               '2. _______\n' +
+                               '3. _______\n' +
+                               'Introduce data set number: '))
+
+    # obtains loaders for training and testing the model
+    if dataset_choice == 1:
+        _, testing_loader = helper_CIFAR10.get_train_and_test_loader(batch_size=1)
+        classes_number = 10
+    else:
+        raise Exception('Not Supported')
+
+    path = input('Introduce model PATH: ')
+
+    # creates model and loads data
+    nn_model = nn_CIFAR10.SimpleNN()
+    nn_model.load_state_dict(torch.load(path))
+
+    # test the model
+    prediction_matrix = test_model(nn_model, testing_loader, classes_number)
+    print(prediction_matrix)
+
+
+if __name__ == '__main__':
+    main()
---
+++ CIFAR_Classifier/NN_modelTrainer.py
@@ -0,0 +1,83 @@
+# https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import helper_CIFAR10
+import SimpleNN as nn_CIFAR10
+import time
+from pathlib import Path
+
+
+device_cuda = torch.device('cuda:0') if (torch.cuda.is_available()) else None
+
+
+def train_model(nn_model: nn.Module, training_loader, epoch_number):
+
+    print(f'Training model with {epoch_number} epochs...')
+    last = time.time()
+
+    # defines optimizer
+    optimizer = optim.SGD(nn_model.parameters(), lr=0.001, momentum=0.9)
+
+    # defines criterion
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(epoch_number):  # trains multiple epochs
+
+        for load in training_loader:
+            # Cleanup: sets the model's parameters' gradients to zero
+            optimizer.zero_grad()
+
+            # Input: unloads input data and label
+            if device_cuda:
+                inputs, labels = load[0].to(device_cuda), load[1].to(device_cuda)
+            else:
+                inputs, labels = load
+
+            # Prediction: generates output
+            output = nn_model(inputs)
+
+            # Optimization: calculates loss and gradients to optimize model
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+        now = time.time()
+        print(f'- epoch #{epoch + 1} completed!\tTime: {(now - last):.2f}')
+        last = time.time()
+
+    print(f'Finished Training')
+
+
+def main():
+    print('NN_modelTrainer.py\n')
+    dataset_choice = int(input('Select a data set\n' +
+                               '1. CIFAR10\n' +
+                               '2. _______\n' +
+                               '3. _______\n' +
+                               'Introduce data set number: '))
+
+    # obtains loaders for training and testing the model
+    if dataset_choice == 1:
+        training_loader, _ = helper_CIFAR10.get_train_and_test_loader(batch_size=4)
+    else:
+        raise Exception('Not Supported')
+
+    model_name = input('Introduce model name: ')
+    epoch_number = int(input('Introduce numbers of epoch: '))
+
+    # creates model and trains it
+    nn_model = nn_CIFAR10.SimpleNN()
+    if device_cuda:
+        nn_model.to(device_cuda)
+
+    train_model(nn_model, training_loader, epoch_number)
+
+    # saves model
+    relative_path = f'./models/{model_name}.pth'
+    torch.save(nn_model.state_dict(), relative_path)
+    print(f'{model_name} was saved at {Path(relative_path).resolve()}')
+
+
+if __name__ == '__main__':
+    main()
